eddec3e8a1c9c741ccd9950170ce4ccfc8e3a114
galet0 <galeto.miteva@gmail.com>
2019-05-12 16:53:13 +0300

    OnlineStore Exam

---
+++ Module_3/08_Exam_12_05_2019_OnlineStore/Exam_12_05_19_ITKarieri_OnlineStore/Order.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore
+{
+    class Order
+    {
+        private int orderNumber;
+        private List<Product> products;
+        private static int ordersCount;
+
+        public Order(int orderNumber, List<Product> products)
+        {
+            this.OrderNumber = orderNumber;
+            this.products = products;
+            Order.ordersCount++;
+        }
+
+        public int OrderNumber
+        {
+            get { return this.orderNumber; }
+            private set
+            {
+                this.orderNumber = value;
+            }
+        }
+
+        public static int OrdersCount
+        {
+            get { return Order.ordersCount; }
+        }
+
+        public void AddProduct(Product product)
+        {
+            this.products.Add(product);
+        }
+
+        public double GetOrderTotalPrice()
+        {
+            double totalPrice = 0;
+            foreach (Product product in products)
+            {
+                totalPrice += product.Price;
+            }
+
+            return totalPrice;
+        }
+
+        public double GetDiscountedProductsTotalPrice()
+        {
+            return this.products.Where(p => p.IsOnPromotion == true).Sum(p => p.Price);
+        }
+
+        public int GetDiscountedProductsCount()
+        {
+            int count = 0;
+            foreach (Product product in products)
+            {
+                if (product.IsOnPromotion)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendFormat("Order #{0} has the following products:", this.OrderNumber);
+            foreach (Product product in products)
+            {
+                result.Append(Environment.NewLine);
+                result.AppendFormat("### Product -> {0} with price {1:f2}. On promotion: {2}",
+                    product.Name, product.Price, product.IsOnPromotion ? "YES" : "NO");                
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ Module_3/08_Exam_12_05_2019_OnlineStore/Exam_12_05_19_ITKarieri_OnlineStore/Product.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore
+{
+    class Product
+    {
+        private string name;
+        private double price;
+        private bool isOnPromotion;
+
+        public Product(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public Product(string name, double price, bool isOnPromotion)
+        {
+            this.Name = name;
+            this.IsOnPromotion = isOnPromotion;
+            this.Price = price;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if(string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException("Invalid product name!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public double Price
+        {
+            get { return this.price; }
+            private set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("Price should be positive!");
+                }
+                if (this.IsOnPromotion)
+                {
+                    this.price = value * 0.8;
+                }
+                else
+                {
+                    this.price = value;
+                }
+                
+            }
+        }
+
+        public bool IsOnPromotion
+        {
+            get { return this.isOnPromotion; }
+            private set
+            {
+                this.isOnPromotion = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            //Product -> <име> with price <цена>. On promotion <YES/NO>
+            if (this.IsOnPromotion)
+            {
+                return $"Product -> {this.Name} with price {this.Price:f2}. On promotion: YES";
+            }
+            else
+            {
+                return $"Product -> {this.Name} with price {this.Price:f2}. On promotion: NO";
+            }
+            
+        }
+    }
+}

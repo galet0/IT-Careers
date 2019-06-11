f22648e4f6445f2a6de37b78e41de9ef926ce6fd
Galya Miteva <galeto.miteva@gmail.com>
2019-06-11 13:22:57 +0300

    Add files via upload

---
+++ Module 4 - Intro to Algorithms and Data Structures/02_Linear Data Structures/Stack/Stack_ADS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_ADS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<string> stack = new CustomStack<string>();
+            stack.Push("pesho");
+            stack.Push("gosho");
+            stack.Push("tosho");
+
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Count);
+        }
+    }
+}
+ 

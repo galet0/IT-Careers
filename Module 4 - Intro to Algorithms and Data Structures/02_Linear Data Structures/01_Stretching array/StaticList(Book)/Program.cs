889dac9b642e4473cb16cc7ab29639a51282ffec
galet0 <galeto.miteva@gmail.com>
2019-06-16 17:23:23 +0300

    another implementation of list

---
+++ Module 4 - Intro to Algorithms and Data Structures/02_Linear Data Structures/01_Stretching array/StaticList(Book)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticList_Book_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomArrayList list = new CustomArrayList();
+            list.Add(5);
+            Console.WriteLine(list[1]);
+            Console.WriteLine();
+        }
+    }
+}

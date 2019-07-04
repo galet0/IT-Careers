c7586d1412edd332adbbdc2cc3ff5ee5799c550a
galet0 <galeto.miteva@gmail.com>
2019-07-04 15:59:46 +0300

    Train

---
+++ Module 4 - Intro to Algorithms and Data Structures/07_Exams/03_Train_Exam_Preparation/Train/Train.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Train
+{
+    class Train
+    {
+        private int number;
+        private string name;
+        private string type;
+        private int cars;
+
+        public Train(int number, string name, string type, int cars)
+        {
+            this.Number = number;
+            this.Name = name;
+            this.Type = type;
+            this.Cars = cars;
+        }
+
+        public int Number { get; private set; }
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public int Cars { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Number} {this.Name} {this.Type} {this.Cars}";
+        }
+    }
+}
